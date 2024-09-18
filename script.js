3bc8eb86657e0f9637807577b81b6df4466a903d
Sgamb29 <shagamb@gmail.com>
2024-09-17 21:53:27 -0600

    countdown and screen lock added

--- script.js
+++ script.js
@@ -35,6 +35,14 @@ document.getElementById("colorChangeToggle").addEventListener("click", () => {
     }
 });
 
+// Variables for countdown
+let secondsLeft = 0;
+let countdownInterval = null;
+const countdownOutput = document.getElementById("countdownOutput");
+
+
+let timeoutId = null;
+
 async function startTimer(sec) {
     let output = document.getElementById("output");
     if (colorChangeEnabled) {
@@ -45,24 +53,41 @@ async function startTimer(sec) {
         let minorsec = sec > 60 ? "Minute" : "Second";
         let minutes = sec > 60 ? sec / 60 : sec;
         output.innerText = `${minutes} ${minorsec} Timer Started - Hold and Breathe.`;
-        setTimeout(() => {
+
+        // Logic for countdown
+        secondsLeft = sec;
+        countdownOutput.innerText = secondsLeft;
+
+        countdownInterval = setInterval(() => {
+            secondsLeft -= 1;
+            countdownOutput.innerText = secondsLeft;
+        }, 1000);
+
+        // Setting up audioElement for ios
+        const audio = new Audio();
+        audio.src = currentAudio;
+        
+
+        timeoutId = setTimeout(() => {
             output.innerText = "Release.";
             firstPress = true;
             if (audioEnabled) {
-                let player = new Audio(currentAudio);
-                player.play()
+
+                audio.play();
             }
 
             if (colorChangeEnabled) {
                 document.getElementById("container").style.backgroundColor = "lightgreen";
             }
+
+            clearInterval(countdownInterval);
         }, sec * 1000);
     }
 }
 
-function refresh() {
-    window.location.reload();
-}
+// function refresh() {
+//     window.location.reload();
+// }
 
 let extraTimersToggle = document.getElementById("extraTimersToggle");
 
@@ -75,3 +100,49 @@ extraTimersToggle.addEventListener("click", () => {
     
 })
 
+function stopAndReset() {
+    if (timeoutId !== null) {
+        output.innerText = "Release.";
+        firstPress = true; 
+        if (colorChangeEnabled) {
+            document.getElementById("container").style.backgroundColor = "lightgreen";
+        }
+        countdownOutput.innerText = "";
+        clearInterval(countdownInterval);
+        clearTimeout(timeoutId);
+    }
+
+}
+
+
+// Wake lock logic
+
+const screenWake = document.getElementById("screenWake");
+
+let isSupported = false;
+let wakeLock = null;
+const wakeLabel = document.getElementById("wakeLabel");
+
+screenWake.addEventListener("click", async () =>{
+    if (screenWake.checked) {
+        if ("wakeLock" in navigator) {
+            isSupported = true;
+            try {
+                wakeLock = await navigator.wakeLock.request("screen");
+                wakeLabel.innerText = "Wake Lock is active!";
+              } catch (err) {
+                console.log(`${err.name}, ${err.message}`);
+                wakeLabel.innerText = "Wake Lock error, might be battery settings."
+              }
+
+        } else {
+            isSupported = false;
+            document.getElementById("wakeLabel").innerText = "Wake Lock Not Supported";
+        }
+    } else {
+        wakeLock.release().then(() => {
+            wakeLock = null;
+            wakeLabel.innerText = "Keep Screen Awake";
+          });
+    }
+})
