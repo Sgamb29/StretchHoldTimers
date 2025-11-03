da5f09b35544035bba7f98d13052002b68a0308b
Sgamb29 <shagamb@gmail.com>
2025-11-03 11:26:30 -0700

    text shorten

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ async function startTimer(sec) {
         firstPress = false;
         let minorsec = sec > 60 ? "Minute" : "Second";
         let minutes = sec > 60 ? sec / 60 : sec;
-        output.innerText = `${minutes} ${minorsec} Timer Started - Hold and Breathe.`;
+        output.innerText = `${minutes} ${minorsec} Timer Started.`;
 
         // Setting up audioElement for ios
         const audio = new Audio();
