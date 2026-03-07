b4607b57867a1e8ecfda76fb0d5f76466a3e0853
Sgamb29 <shagamb@gmail.com>
2026-03-07 16:40:23 -0700

    added sequence page

--- script2.js
+++ script2.js
@@ -1,5 +1,175 @@
+class Clock {
+    intervalId = null;
+    displayElement = null;
+    msInterval = 1000;
+    justSeconds = false;
+    needsResume = false;
+    currentSetting = null;
 
-let audioToggle = document.getElementById("audioToggle");
+    start() {}
+
+    display(sec=0) {
+        if (this.displayElement !== null) {
+            if (this.justSeconds) {
+                this.displayElement.innerText = sec;
+            } else {
+                this.displayElement.innerText = this.getTimeStr(sec);
+            }
+        }
+    }
+
+    pause() {
+        this.needsResume = true;
+        this.stop();
+    }
+
+    resume() {
+        if (this.needsResume) {
+            this.start();
+            this.needsResume = false;
+        } 
+    }
+
+    printError() {
+        console.error("Can only use one timer at a time (for now)");
+    }
+
+    getTimeStr(sec) {
+        const mins = parseInt(sec / 60);
+        const hrs = parseInt(mins / 60);
+        const secs = sec - mins * 60 - hrs * 60 * 60;
+        const mStr = mins < 10 ? `0${mins}` : `${mins}`;
+        const hStr = hrs < 10 ? `0${hrs}` : `${hrs}`;
+        const sStr = secs < 10 ? `0${secs}` : `${secs}`;
+        return `${hStr}:${mStr}:${sStr}`;
+    
+    }
+
+    setDisplayElement(el) {
+        this.displayElement = el;
+    }
+
+    setUseJustSeconds(bool) {
+        this.justSeconds = bool === true ? true : false;
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    reset() {}
+
+    setStartResetPauseResumeButtons(startResetPauseResume=[]) { 
+        let count = 1;
+        startResetPauseResume.forEach((el) => {
+            switch (count) {
+                case 1:
+                    el.addEventListener("click", () => {this.start() });
+                    break;
+                case 2:
+                    el.addEventListener("click", () => {this.reset() });
+                    break;
+                case 3:
+                    el.addEventListener("click", () => {this.pause() });
+                    break;
+                case 4:
+                    el.addEventListener("click", () => {this.resume() });
+                    break;
+                default:
+                    console.log("count of elements out of bounds.");
+                    break;
+            }
+            count += 1;
+
+        });
+
+    }
+
+}
+class Countdown extends Clock {
+    countdownSeconds = 0;
+    endFunction = null;
+    countdownState = null;
+    currentSetting = "countdown";
+
+    setEndFunction(func=null) {
+        this.endFunction = func;
+    }
+
+    setSeconds(sec) {
+        this.countdownSeconds = sec;
+    }
+
+    start() {
+        // if (this.intervalId !== null) {
+        //     this.printError();
+        //     return;
+        // }
+        let currentSecs;
+        // Resume case
+        currentSecs = this.needsResume ? this.countdownState : this.countdownSeconds;
+        // 
+        this.display(currentSecs);
+        this.countDownState = currentSecs;
+        this.intervalId = setInterval(() => {
+            currentSecs -= 1;
+            this.countdownState = currentSecs;
+            this.display(currentSecs);
+            if (currentSecs <= 0) {
+                this.stop();
+                if (this.endFunction !== null) {
+                    this.endFunction();
+                }
+            }
+        }, this.msInterval);
+    }
+
+    reset() {
+        this.stop();
+        this.countDownState = this.countdownSeconds;
+        this.display(this.countdownSeconds);
+    }
+
+}
+class Tone {
+
+    audioCtx;
+    osc;
+    sweepEnv;
+
+    initialize() {
+        this.audioCtx = new AudioContext();
+        this.osc = new OscillatorNode(this.audioCtx);
+        this.sweepEnv = new GainNode(this.audioCtx);
+    }
+
+    playHighTone() {
+        this.playSweep(680);
+
+    }
+
+    playLowTone() {
+        this.playSweep(340);
+
+    }
+
+    playSweep(freq) {
+        this.initialize(); 
+        this.osc.type = "sine";
+        this.osc.frequency.setValueAtTime(freq, this.audioCtx.currentTime);
+        // this.osc.frequency.setValueAtTime(freq - 50, 0.3);
+
+        this.sweepEnv.gain.setValueAtTime(2, 0);
+        // this.sweepEnv.gain.linearRampToValueAtTime(1, .2);
+        this.sweepEnv.gain.linearRampToValueAtTime(0, .4);
+
+        this.osc.connect(this.sweepEnv).connect(this.audioCtx.destination)
+        this.osc.start(0);
+        this.osc.stop(1);
+    }
+}
+const audioToggle = document.getElementById("audioToggle");
 let audioEnabled = true;
 
 audioToggle.addEventListener("click", (e) => {
@@ -7,153 +177,205 @@ audioToggle.addEventListener("click", (e) => {
 })
 
 
-let four = document.getElementById("fourMinute");
-let eight = document.getElementById("eightMinute");
+const holdInput = document.getElementById("holdSec");
+const restInput = document.getElementById("restSec");
+const setInput = document.getElementById("numSets")
+const startBtn = document.getElementById("startButton");
+const output = document.getElementById("output");
+const indicator = document.getElementById("indicator");
 
-let lengthOpts = [four, eight];
-let currentLength = 4;
+let numSets = 1;
+let sequence = [];
+let masterSequence = [];
+let index = 0;
+let totalStreches = 0;
+let restSecs = 5;
 
-for (opt of lengthOpts) {
-    opt.addEventListener("click", (e) => {
-        currentLength = parseInt(e.target.value);
-    })
+const timer = new Countdown();
+timer.setDisplayElement(output);
+timer.setEndFunction(nextStretch);
+const tone = new Tone();
+
+let isStretching = false;
+let alreadyStarted = false;
+
+function startSequence() {
+    if (alreadyStarted) {
+        return;
+    }
+    if (!updateValues()) {
+        return;
+    }
+    timer.setSeconds(restSecs);
+    timer.start();
+    alreadyStarted = true;
+    indicator.innerText = "Get ready.";
 }
 
-let accumulatedTime = 0;
+function resetVariables() {
+    alreadyStarted = false;
+    isStretching = false;
+    index = 0;
+    sequence = [];
+    masterSequence = [];
+}
 
-async function autoplaySequence(index) {
-    let mainImage = document.getElementById("mainImage");
-    let output = document.getElementById("output");
-    let btn = document.getElementById("autoPlayStartButton");
-    let photographer = document.getElementById("photographer");
+function nextStretch() {
+    if (audioEnabled) {
+        tone.playLowTone();
 
-    btn.disabled = true;
-    const sequence = [
-        {
-            "img": "assets/photos/pexels-elina-fairytale-3822191.jpg",
-            "time": 60,
-            "text": "Seated Forward Fold 60 Seconds",
-            "by": "Photo by Elina Fairytale",
-        },
-        {
-            "img": "assets/photos/pexels-miriam-alonso-7592479.jpg",
-            "time": 60,
-            "text": "Butterfly Pose 60 Seconds",
-            "by": "Photo by Miriam Alonso",
-        },
-        {
-            "img": "assets/photos/pexels-ketut-subiyanto-4436298.jpg",
-            "time": 30,
-            "text": "One-Leg Forward Bend Left 30 Seconds",
-            "by": "Photo by Ketut Subiyanto"
-        },
-        {
-            "img": "assets/photos/pexels-ketut-subiyanto-4436298.jpg",
-            "time": 30,
-            "text": "SWITCH! One-Leg Forward Bend Right 30 Seconds",
-            "by": "Photo by Ketut Subiyanto",
-        },
-        {
-            "img": "assets/photos/pexels-vlada-karpovich-4534689.jpg",
-            "time": 60,
-            "text": "Downward Dog 60 Seconds",
-            "by": "Photo by Vlada Karpovich",
-        },
-        {
-            "img": "assets/photos/pexels-marta-wave-6453939.jpg",
-            "time": 30,
-            "text": "Low Lunge 30 Seconds",
-            "by": "Photo by Marta Wave",
-        },
-        {
-            "img": "assets/photos/pexels-marta-wave-6453939.jpg",
-            "time": 30,
-            "text": "SWITCH! Low Lunge, Other leg, 30 Seconds",
-            "by": "Photo by Marta Wave",
-        },
-        {
-            "img": "assets/photos/pexels-elina-fairytale-3822088.jpg",
-            "time": 60,
-            "text": "Standing Forward Fold 60 Seconds",
-            "by": "Photo by Elina Fairytale",
-        },
-        {
-            "img": "assets/photos/pexels-rfstudio-3820430.jpg",
-            "time": 60,
-            "text": "Reach for the Sky 60 seconds",
-            "by": "Photo by RF Studio",
-        },
-        {
-            "img": "assets/photos/pexels-marta-wave-6454068.jpg",
-            "time": 60,
-            "text": "Wide Legged Forward Fold 60 seconds",
-            "by": "Photo by Marta Wave",
-        },
-    ];
-
-    endingImage = {
-        "img": "assets/photos/pexels-mikhail-nilov-6707077.jpg",
-        "time": 10,
-        "text": "Session Finished!",
-        "by": "Photo by Mikhail Nilov",
     }
+    // Starting a stretch
+    if (!isStretching) {
+        isStretching = true;
+        timer.setSeconds(parseInt(masterSequence[index]));
+        timer.start();
+        indicator.innerText = `Stretch ${index + 1}/${totalStreches}`;
 
-    // Button is pressed, starting image / data is loaded
-
-    btn.innerText = "Autoplay Started"
-    mainImage.src = sequence[index]["img"];
-    output.innerText = sequence[index]["text"];
-    photographer.innerText = sequence[index]["by"];
-    let lastTime = sequence[index]["time"];
-
-
-
-
-    let intervalId = setInterval(() => {
-        if (audioEnabled) {
-            let player = new Audio("assets/drum-sound.mp3");
-            player.play()
-        }
-        accumulatedTime += sequence[index]["time"];
-        console.log(accumulatedTime);
-
+    } else {
+        // Starting a rest - transitiion
+        isStretching = false;
         index += 1;
-        if (index < sequence.length & accumulatedTime < currentLength * 60) {
-            let currentTime = sequence[index]["time"];
-            checkTimeDifference(intervalId, lastTime, currentTime, index);
-
-        } else {
-            output.innerText = "Sequence Finished!";
-            mainImage.src = endingImage["img"];
-            photographer.innerText = endingImage["by"];
-            btn.innerText = "Start Autoplay";
-            index = 0;
-            clearInterval(intervalId);
-            btn.disabled = false;
-
-            accumulatedTime = 0;
-
+        // Finish sequence case
+        if (index === totalStreches) {
+            tone.playHighTone();
+            indicator.innerText = "Sequence finished.";
+            resetVariables();
             return;
         }
-        mainImage.src = sequence[index]["img"];
-        output.innerText = sequence[index]["text"];
-        photographer.innerText = sequence[index]["by"];
-
-
-        lastTime = sequence[index]["time"];
-        
-        
-        
-    }, sequence[index]["time"] * 1000);
-
-
-        
-}
-
-function checkTimeDifference(iid, lastTime, currentTime, index) {
-    if (lastTime != currentTime) {
-        clearInterval(iid);
-        autoplaySequence(index);
-
+        if (restSecs === 0) {
+            nextStretch();
+        } else {
+            timer.setSeconds(restSecs);
+            timer.start()
+            indicator.innerText = "Rest - Transition.";
+        }
     }
 }
+
+function isValidSequence(s) {
+    let isValid = true;
+    s.forEach((i) => {
+        if (isNaN(parseInt(i))) {
+            isValid = false;
+        }
+    })
+    return isValid;
+}
+
+function pause() {
+    const pauseButton = document.getElementById("pauseButton");
+    if (!alreadyStarted) {
+        return;
+    }
+    if (timer.needsResume) {
+        timer.resume();
+        pauseButton.innerText = "Pause";
+    } else {
+        timer.pause();
+        pauseButton.innerText = "Resume";
+    }
+}
+
+function reset() {
+    timer.stop();
+    resetVariables();
+    output.innerText = "Sequence restarted.";
+    indicator.innerText = "";
+}
+
+function isValidInputs() {
+
+    // Set input checking
+    if (isNaN(parseInt(setInput.value))) {
+        output.innerText = "Input error: sets input isn't a number.";
+        return false;
+    } else if (parseInt(setInput.value) <= 0) {
+        output.innerText = "Input error: sets can't be 0.";
+        return false;
+    }
+
+    // Rest input checking
+    if (isNaN(parseInt(restInput.value))) {
+        output.innerText = "Input error: rest seconds isn't a number.";
+        return false;
+    } else if (parseInt(restInput.value) < 0) {
+        output.innerText = "Input error: rest seconds has to be 0 or more."
+        return false;
+    }
+
+    // Sequence input checking
+    if (holdInput.value === "") {
+        output.innerText = "Input error: sequence stretch times empty.";
+        return false;
+    }
+
+    if (!isValidSequence(sequence)) {
+        output.innertext = "Input error: stretch seconds aren't in the right format.";
+        return false;
+    }
+
+    return true;
+}
+
+function updateValues() {
+    const sequenceStr = holdInput.value;
+    sequence = sequenceStr.split("-");
+    if (!isValidInputs()) {
+        return false;
+    }
+    numSets = parseInt(setInput.value);
+    restSecs = parseInt(restInput.value);
+    generateMaster();
+    return true;
+}
+
+function generateMaster() {
+    for (let i = 0; i < numSets; i++) {
+        sequence.forEach((el) => {
+            masterSequence.push(el);
+        })
+    }
+    totalStreches = masterSequence.length;
+}
+
+
+// Wake lock logic
+const screenWake = document.getElementById("screenWake");
+
+let isSupported = false;
+let wakeLock = null;
+const wakeLabel = document.getElementById("wakeLabel");
+
+
+screenWake.addEventListener("click", async () =>{
+    if (screenWake.checked) {
+        if ("wakeLock" in navigator) {
+            isSupported = true;
+
+            try { 
+                wakeLock = await navigator.wakeLock.request("screen");           
+                wakeLabel.innerText = "Wake Lock is active!";
+              } catch (err) {
+                console.log(`${err.name}, ${err.message}`);
+                wakeLabel.innerText = "Wake Lock error, might be battery settings."
+              }
+
+        } else {
+            isSupported = false;
+            document.getElementById("wakeLabel").innerText = "Wake Lock Not Supported";
+        }
+    } else {
+        wakeLock.release().then(() => {
+            wakeLock = null;
+            wakeLabel.innerText = "Keep Screen Awake";
+          });
+    }
+})
+
+// Reset wake lock if navigated away and back.
+document.addEventListener("visibilitychange", () => {
+    if (screenWake.checked) {
+        screenWake.click();
+    }
+})
